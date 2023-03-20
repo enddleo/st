6af87ff6cfacc1d1d4a3f77e4527fc1a4e430808
enddleo <enddleo@outlook.com>
2023-03-20 18:59:48 +0800

    final2

--- config.h
+++ config.h
@@ -123,6 +123,7 @@ static const char *colorname[] = {
     /* special colors */
     [256] = "#040405", /* background */
     [257] = "#f8f8f2", /* foreground */
+    [258] = "#18efd9",
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
@@ -138,7 +139,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
